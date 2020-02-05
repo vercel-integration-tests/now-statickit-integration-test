f2efa9dfa67361afb6d7b5fbe8c3e601e639a1f8
William Li <williamli@bbi.io>
2020-02-05 19:22:41 +0800

    testing statickit

--- pages/api/date.js
+++ pages/api/date.js
@@ -1,4 +1,9 @@
-export default (req, res) => {
+import { helloWorld, createClient } from '@statickit/functions';
+
+export default async (req, res) => {
+  let client = createClient({ site: "9d6fb7039a1f" });
+  await helloWorld(client, { subject: "Hello world!" });
+
   const date = new Date()
     .toISOString()
     .replace(/T/, ' ')
